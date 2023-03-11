b157199ad24099282e2a10322a9f14f248bffe43
Mohamed BENDAOUD <benmoham@e1r6p5.clusters.42paris.fr>
2023-03-11 17:33:10 +0100

    all services are functional except the db which is to be adjusted

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+// Décorateur qui permet de définir un module NestJS et de le rendre disponible pour d'autres modules
+// Un module NestJS est une classe qui est décorée avec le décorateur @Module()
+import { AppController } from './app.controller'; 
+// ecouter une requête HTTP et renvoyer une réponse HTTP
 import { AppService } from './app.service';
+// Fournir des services à nos contrôleurs et à nos autres services
 
 @Module({
   imports: [],
